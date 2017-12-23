f82ff8e1c308992db5cb06471cdde869967be0de
MrAaronCasanova <1boris231@gmail.com>
2017-12-23 00:35:37 -0800

    added a view more tools to the box

--- js/casy.js
+++ js/casy.js
@@ -1,5 +1,21 @@
+/* --------------------------------------------
+Main Filter Functionality
+-------------------------------------------- */
 var options = {
   valueNames: ['html', 'css', 'javascript', 'git'],
 };
 
 var refList = new List('references', options);
+
+/* --------------------------------------------
+Transform
+-------------------------------------------- */
+
+/* ------ Translate ------ */
+
+$('#button').click(function () {
+  $('.noOne').toggleClass('crazyOne');
+  $('.noTwo').toggleClass('crazyTwo');
+  $('.noThree').toggleClass('crazyThree');
+  $('.noFour').toggleClass('crazyFour');
+});
