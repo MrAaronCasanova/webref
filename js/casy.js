e4c3e1325171f9476a69081e910e5c9ba3642318
MrAaronCasanova <1boris231@gmail.com>
2018-01-28 14:46:23 -0800

    added content to js sections, about to start advanced web dev bootcamp

--- js/casy.js
+++ js/casy.js
@@ -634,7 +634,7 @@ Delete This - Tutorial Project Exercise
 // Vehicle.prototype.turnOn = function () {
 //   return this.isRunning = true;
 // };
-// 
+//
 // Vehicle.prototype.turnOff = function () {
 //   return this.isRunning = false;
 // };
@@ -644,3 +644,74 @@ Delete This - Tutorial Project Exercise
 //     return 'beep';
 //   }
 // };
+
+/* ------ Closures ------ */
+
+// function outer() {
+//   var data = 'closures are ';
+//   return function inner() {
+//     var innerData = 'awesome';
+//     return data + innerData;
+//   };
+// }
+//
+// outer()(); // closures are awesome
+//
+// function outer(a) {
+//   return function inner(b) {
+//     // the inner function is making use of the variable 'a'
+//     // which was defined in an outer funtion called 'outer'
+//     // and by the time this is called, that outer function
+//     // has returned this function called 'inner' is a closure
+//     return a + b;
+//   };
+// }
+//
+// outer(5)(5); // 10
+//
+// var storeOuter = outer(5);
+// storeOuter(10); //15
+
+// function counter() {
+//   var count = 0;
+//   return function () {
+//     return ++count;
+//   };
+// }
+//
+// counter1 = counter();
+// counter1(); // 1
+// counter1(); // 2
+//
+// counter2 = counter();
+// counter2(); // 1
+// counter2(); // 2
+//
+// counter1(); // 3 this is not affected by counter2!
+//
+// count; // ReferenceError: count is not defined - because it is private!
+//
+// function classRoom() {
+//   var instructors = ['Colt', 'Elie']; // private var
+//   return {
+//     getInstructors: function () {
+//       return instructors;
+//     },
+//
+//     addInstructor: function (instructor) {
+//       instructors.push(instructor);
+//       return instructors;
+//     },
+//   };
+// }
+//
+// course1 = classRoom();
+// course1.getInstructors(); // ['Colt', 'Elie']
+// course1.getInstructors('Ian'); // ['Colt', 'Elie', 'Ian']
+// course1.getInstructors(); // ['Colt', 'Elie', 'Ian']
+//
+// course2 = classRoom();
+// course2.getInstructors(); // ['Colt', 'Elie'] - not affected by course1
+//
+// // We also have NO access to the instructors variables
+// // which makes it private - no one can modify it...
