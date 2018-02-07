e1e4af027356d34c366f0a8fb8ea2367fa6ce89b
MrAaronCasanova <1boris231@gmail.com>
2018-02-07 11:05:14 -0800

    added pre/code tag indentation modification

--- public/js/casy.js
+++ public/js/casy.js
@@ -9,6 +9,17 @@ var options = {
 var refList = new List('references', options);
 
 /* --------------------------------------------
+Pre/Code Tags Modification
+-------------------------------------------- */
+
+// ignores starting indentation when using pre/code tags
+$('pre code').each(function () {
+  var html = $(this).html();
+  var pattern = html.match(/\s*\n[\t\s]*/);
+  $(this).html(html.replace(new RegExp(pattern, 'g'), '\n'));
+});
+
+/* --------------------------------------------
 Keyframe Animation
 -------------------------------------------- */
 
