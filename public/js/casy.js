fe768ea3fcc8e84a46c0f77f25fe7e1070d70d29
MrAaronCasanova <1boris231@gmail.com>
2018-02-18 16:55:29 -0800

    call, apply, bind tutorials complete for now

--- public/js/casy.js
+++ public/js/casy.js
@@ -1604,3 +1604,72 @@ ES6
 // console.log(guess(3));
 // console.log(guess(4));
 // console.log(guess(5));
+
+/* ------ Call, Apply, Bind ------ */
+
+// function arrayFrom(arrayLikeObject) {
+//   return [].slice.call(arrayLikeObject);
+// }
+
+// ---------------------------------------------
+
+// function sumEvenArguments() {
+//   var newArgs = [].slice.call(arguments);
+//   return newArgs.reduce(function (acc, next) {
+//     if (next % 2 === 0) {
+//       return acc + next;
+//     }
+//
+//     return acc;
+//   }, 0);
+// }
+//
+// console.log(sumEvenArguments(1, 2, 3, 4));
+
+// ---------------------------------------------
+
+// function invokeMax(fn, num) {
+//   let max = 0;
+//   return function () {
+//     if (max >= num) {
+//       return 'Maxed Out!';
+//     }
+//
+//     max++;
+//     return fn.apply(this, arguments);
+//   };
+// }
+
+// ---------------------------------------------
+
+// function once(fn, thisArg) {
+//   let hasBeenCalled = false;
+//   return function () {
+//     if (!hasBeenCalled) {
+//       hasBeenCalled = true;
+//       return fn.apply(thisArg, arguments);
+//     }
+//   };
+// }
+
+// ---------------------------------------------
+
+// function bind(fn, thisArg) {
+//   let outerArgs = [].slice.call(arguments, 2);
+//   return function () {
+//     let innerArgs = [].slice.call(arguments);
+//     let allArgs = outerArgs.concat(innerArgs);
+//     return fn.apply(thisArg, allArgs);
+//   };
+// }
+
+// ---------------------------------------------
+
+// function flip() {
+//   let outerArgs = [].slice.call(arguments, 2);
+//   return function () {
+//     let innerArgs = [].slice.call(arguments);
+//     let allArgs = outerArgs.concat(innerArgs).slice(0, fn.length);
+//     return fn.apply(thisArg, allArgs.reverse());
+//   };
+// }
