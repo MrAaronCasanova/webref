049a8fe7a631762fa4230901db4026d5bea15439
MrAaronCasanova <1boris231@gmail.com>
2018-02-22 19:09:25 -0800

    promise exercises

--- public/js/casy.js
+++ public/js/casy.js
@@ -1927,3 +1927,45 @@ ES6
 //
 //   return count;
 // }
+
+/* ------ Promises Exercises ------ */
+
+// function getMostFollowers(...usernames) {
+//   let baseUrl = 'https://api.github.com/users/';
+//   let urls = usernames.map(username => $.getJSON(baseUrl + username));
+//   return Promise.all(urls).then(function (data) {
+//     let max = data.sort((a, b) => a.followers < b.followers)[0];
+//     return `${max.name} has the most followers with ${max.followers}`;
+//   });
+// }
+//
+// getMostFollowers('elie', 'tigarcia', 'colt')
+// .then(function (data) {
+//   console.log(data);
+// });
+//
+// function starWarsString(id) {
+//   let str = '';
+//   return $.getJSON(`https://swapi.co/api/people/${id}`)
+//   .then(function (data) {
+//     str += `${data.name} is featured in `;
+//     let filmData = data.films[0];
+//     return $.getJSON(filmData);
+//   })
+//   .then(function (res) {
+//     str += `${res.title}, directed by ${res.director} `;
+//     let planetData = res.planets[0];
+//     return $.getJSON(planetData);
+//   })
+//   .then(function (res) {
+//     str += `and it takes place on ${res.name}`;
+//     return str;
+//   })
+//   .then(function (finalString) {
+//     return finalString;
+//   });
+// }
+//
+// starWarsString(4).then(function (data) {
+//   console.log(data);
+// });
